13dfa198a315a1a2110a686738ca5b223429f206
Soszka <bsocha1998@gmail.com>
2024-04-11 07:57:56 +0200

    responsive navigation added to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavigationComponent], 
+  imports: [NavigationComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular-progressCounter';
+  title = 'Licznik Progressu';
 }
---
+++ src/app/shared/navigation/expanded-nav/expanded-nav.component.ts
@@ -0,0 +1,65 @@
+import { Component, Input, Output, EventEmitter  } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { faDumbbell, faSignInAlt, faChartLine, faFire, faHome} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { group } from '@angular/animations';
+
+@Component({
+  selector: 'app-expanded-nav',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './expanded-nav.component.html',
+  styleUrl: './expanded-nav.component.scss',
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({
+        height: '0',
+        opacity: '0',
+      })),
+      state('expanded', style({
+        height: '100vh',
+        opacity: '1',
+      })),
+      transition('collapsed => expanded', [
+        group([
+          animate('400ms ease-out', style({
+            height: '100vh',
+          })),
+          animate('400ms 200ms ease-out', style({ 
+            opacity: '1',
+          })),
+        ]),
+      ]),
+      transition('expanded => collapsed', [
+        group([
+          animate('200ms ease-out', style({
+            opacity: '0',
+          })),
+          animate('400ms ease-out', style({
+            height: '0',
+          })),
+        ]),
+      ]),
+    ])
+  ],
+})
+
+export class ExpandedNavComponent {
+
+  faHome = faHome;
+  faDumbbell = faDumbbell;
+  faSignInAlt = faSignInAlt;
+  faChartLine = faChartLine;
+  faFire = faFire; 
+
+  @Input() isNavExpanded!: boolean;
+  @Output() clickedLinkEvent = new EventEmitter<void>();
+
+  get stateName(): string {
+    return this.isNavExpanded ? 'expanded' : 'collapsed';
+  }
+
+  clickedLink() {
+    this.clickedLinkEvent.emit();
+  }
+}
