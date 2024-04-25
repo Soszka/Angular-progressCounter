dd04f8246113a0c4911462689d5a8736e5e280b2
Soszka <bsocha1998@gmail.com>
2024-04-25 11:41:15 +0200

    trening cards added

--- src/app/training/training-exercises/training-exercises.component.ts
+++ src/app/training/training-exercises/training-exercises.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-training-exercises',
   standalone: true,
-  imports: [MatTabsModule, ButtonComponent ],
+  imports: [ 
+    MatTabsModule, 
+    ButtonComponent, 
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './training-exercises.component.html',
   styleUrl: './training-exercises.component.scss'
 })
