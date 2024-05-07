4dbecfbcedee39addac085eb949ee60d6d33f71a
Bartlomiej Socha <batlomiej.socha@mtupolska.com>
2024-05-07 11:26:13 +0200

    training dialogs basic structure added

---
+++ src/app/training/training-dialogs/remove-exercise-postion-dialog/remove-exercise-postion-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-exercise-postion-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './remove-exercise-postion-dialog.component.html',
+  styleUrl: './remove-exercise-postion-dialog.component.scss'
+})
+export class RemoveExercisePostionDialogComponent {
+
+}
