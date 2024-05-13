773e124cea1337c365c09742d040c706db3eb6e0
Bartlomiej Socha <batlomiej.socha@mtupolska.com>
2024-05-13 08:25:55 +0200

    new mixins added and styles refactored

--- src/app/training/training-description/training-description.component.ts
+++ src/app/training/training-description/training-description.component.ts
@@ -12,6 +12,11 @@ import { ModifyTrainingDialogComponent } from '../training-dialogs/modify-traini
 })
 export class TrainingDescriptionComponent {
 
+  treiningDescription = `
+    W sekcji z treningami masz możliwość stworzenia swojego indywidualnego planu treningowego.
+    Do każdego z treningów będziesz mógł dodawać ćwiczenia, a następnie zapisywać swoje wyniki.
+    Pamiętaj, że gdy zdecydujesz się usunąć któryś z treningów, cała historia związana z osiągnięciami
+  `
   constructor(public dialog: MatDialog) {}
 
   openModifyDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
