4a86cf01df3e3645210fc96fc41c13f23f413e8e
Soszka <bsocha1998@gmail.com>
2024-05-20 08:06:40 +0200

    modify trainings dialog functons refacotred

--- src/app/store/trainings.store.ts
+++ src/app/store/trainings.store.ts
@@ -1,6 +1,6 @@
 import { signalStore, withState, withMethods, patchState } from '@ngrx/signals';
 import { TrainingDataService } from '../api/training-data.service';
-import { Training, TrainingsState } from '../training/training.model';
+import { Training, TrainingsState, ExerciseDailyData } from '../training/training.model';
 import { inject } from '@angular/core';
 import { Observable, tap, finalize } from 'rxjs';
 
@@ -43,6 +43,44 @@ export const TrainingsStore = signalStore(
         tap(() => this.loadTrainings()),
         finalize(() => patchState(store, { loading: false }))
       );
+    },
+    addExercise(trainingName: string, exerciseName: string) {
+      patchState(store, { loading: true });
+      trainingDataService.addExercise(trainingName, exerciseName).subscribe({
+        next: () => {
+          this.loadTrainings();
+          patchState(store, { loading: false });
+        },
+        error: (err: any) => {
+          patchState(store, { error: err.message, loading: false });
+        }
+      });
+    },
+    deleteExercise(trainingName: string, exerciseName: string): Observable<void> {
+      patchState(store, { loading: true });
+      return trainingDataService.deleteExercise(trainingName, exerciseName).pipe(
+        tap(() => this.loadTrainings()),
+        finalize(() => patchState(store, { loading: false }))
+      );
+    },
+    deleteExercisePosition(trainingName: string, exerciseName: string, date: string): Observable<void> {
+      patchState(store, { loading: true });
+      return trainingDataService.deleteExercisePosition(trainingName, exerciseName, date).pipe(
+        tap(() => this.loadTrainings()),
+        finalize(() => patchState(store, { loading: false }))
+      );
+    },
+    addExercisePosition(trainingName: string, exerciseName: string, newPosition: ExerciseDailyData) {
+      patchState(store, { loading: true });
+      trainingDataService.addExercisePosition(trainingName, exerciseName, newPosition).subscribe({
+        next: () => {
+          this.loadTrainings();
+          patchState(store, { loading: false });
+        },
+        error: (err: any) => {
+          patchState(store, { error: err.message, loading: false });
+        }
+      });
     }
   }))
 );
--- src/app/training/training-dialogs/removing-confirm-dialog/removing-confirm-dialog.component.ts
+++ src/app/training/training-dialogs/removing-confirm-dialog/removing-confirm-dialog.component.ts
@@ -3,6 +3,7 @@ import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dial
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { InfoDialogComponent } from '../info-dialog/info-dialog.component';
 import { TrainingsStore } from '../../../store/trainings.store';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-removing-confirm-dialog',
@@ -14,6 +15,7 @@ import { TrainingsStore } from '../../../store/trainings.store';
 export class RemovingConfirmDialogComponent {
 
   trainingStore = inject(TrainingsStore);
+  router = inject(Router);
 
   constructor(
     public dialogRef: MatDialogRef<RemovingConfirmDialogComponent>, 
@@ -21,15 +23,34 @@ export class RemovingConfirmDialogComponent {
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   onRemove(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.trainingStore.deleteTraining(this.data.trainingName).subscribe(() => {
-      const information = this.data.messages.successMessage;
-      this.dialogRef.close();
-      this.dialog.open(InfoDialogComponent, {
-        width: '600px',
-        data: { information },
-        enterAnimationDuration,
-        exitAnimationDuration,
-      });
+    switch (this.data.type) {
+      case 'training':
+        this.trainingStore.deleteTraining(this.data.trainingName).subscribe(() => {
+          this.onSuccess(this.data.messages.successMessage, enterAnimationDuration, exitAnimationDuration);
+        });
+        break;
+      case 'exercise':
+        this.trainingStore.deleteExercise(this.data.trainingName, this.data.exerciseName).subscribe(() => {
+          this.router.navigate(['/training']);  
+          this.onSuccess(this.data.messages.successMessage, enterAnimationDuration, exitAnimationDuration);
+        });
+        break;
+      case 'position':
+        this.trainingStore.deleteExercisePosition(this.data.trainingName, this.data.exerciseName, this.data.date).subscribe(() => {
+          this.onSuccess(this.data.messages.successMessage, enterAnimationDuration, exitAnimationDuration);
+        });
+        break;
+    }
+  }
+  
+  onSuccess(message: string, enterAnimationDuration: string, exitAnimationDuration: string) {
+    const information = message;
+    this.dialogRef.close();
+    this.dialog.open(InfoDialogComponent, {
+      width: '600px',
+      data: { information },
+      enterAnimationDuration,
+      exitAnimationDuration,
     });
   }
   
