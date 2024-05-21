46c0aa15ca7127b98530086a2d0290bb2b9364aa
Bartlomiej Socha <batlomiej.socha@mtupolska.com>
2024-05-21 12:05:28 +0200

    loading spinner added to appliaction

--- src/app/loading-spinner/loading-spinner.component.ts
+++ src/app/loading-spinner/loading-spinner.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { inject } from '@angular/core';
+import { TrainingsStore } from '../store/trainings.store';
 
 @Component({
   selector: 'app-loading-spinner',
   standalone: true,
-  imports: [],
+  imports: [ MatProgressSpinnerModule ],
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.scss'
 })
 export class LoadingSpinnerComponent {
-
+  store = inject(TrainingsStore);
+  isLoading = this.store.loading;
 }
