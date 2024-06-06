b5432dbb63d69acb618bcc358bbd697bb40b9f7a
Bartlomiej Socha <batlomiej.socha@mtupolska.com>
2024-06-06 12:53:59 +0200

    responsiveness added to training-edit component

--- src/app/shared/services/dialog.service.ts
+++ src/app/shared/services/dialog.service.ts
@@ -1,28 +1,27 @@
 import { Injectable, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { fromEvent } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogService {
   private dialog = inject(MatDialog);
-  private originalHeight: number = window.innerHeight;
+  private originalWidth: number = window.innerWidth;
 
   constructor() {
     this.initResizeListener();
   }
 
   private initResizeListener(): void {
-    window.addEventListener('resize', () => {
-      this.onResize();
-    });
+    fromEvent(window, 'resize').subscribe(() => this.onResize());
   }
 
   private onResize(): void {
-    const currentHeight = window.innerHeight;
-    const resizeDifference = this.originalHeight - currentHeight;
-    if (resizeDifference < 100) {
+    const currentWidth = window.innerWidth;
+    if (this.originalWidth !== currentWidth) {
       this.closeAllDialogs();
+      this.originalWidth = currentWidth; 
     }
   }
 
--- src/app/training/training-dialogs/add-exercise-position-dialog/add-exercise-position-dialog.component.ts
+++ src/app/training/training-dialogs/add-exercise-position-dialog/add-exercise-position-dialog.component.ts
@@ -12,7 +12,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { TrainingService } from '../../training.service';
 import { TrainingsStore } from '../../../store/trainings.store';
 import { ExerciseDailyData } from '../../training.model';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-exercise-position-dialog',
@@ -26,7 +27,8 @@ import { ExerciseDailyData } from '../../training.model';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatIconModule
   ],
   templateUrl: './add-exercise-position-dialog.component.html',
   styleUrl: './add-exercise-position-dialog.component.scss'
@@ -69,54 +71,54 @@ export class AddExercisePositionDialogComponent {
     const date = this.firstFormGroup.value.firstCtrl;
     const weight = this.secondFormGroup.value.secondCtrl;
     const repetitions = this.thirdFormGroup.value.thirdCtrl;
-
+  
     if (!date || !this.isValidDate(date)) {
-      const information = "Wprowadź prawidłową datę";
-      this.dialog.open(InfoDialogComponent, {
-        width: '600px',
-        data: { information },
-        enterAnimationDuration,
-        exitAnimationDuration,
-      });
+      this.showInfoDialog("Wprowadź prawidłową datę", enterAnimationDuration, exitAnimationDuration);
       return;
     }
-
+  
     const formattedDate = new Date(date).toLocaleDateString('en-CA'); 
     const currentExercise = this.trainingService.getCurrentExercise();
     if (currentExercise.dailyData.some(data => data.date === formattedDate)) {
-      const information = "Pozycje o takiej dacie już istnieje! Nie można mieć dwóch pozycji z taką samą datą";
-      this.dialog.open(InfoDialogComponent, {
-        width: '600px',
-        data: { information },
-        enterAnimationDuration,
-        exitAnimationDuration,
-      });
+      this.showInfoDialog("Pozycje o takiej dacie już istnieje! Nie można mieć dwóch pozycji z taką samą datą", enterAnimationDuration, exitAnimationDuration);
       return;
     }
-
+  
     if (date && weight && repetitions) {
-      const formattedDate = new Date(date).toLocaleDateString('en-CA');
       const newPosition: ExerciseDailyData = {
         date: formattedDate,
         weight: parseInt(weight, 10),
         repetitions: parseInt(repetitions, 10)
       };
-
+  
       const currentTrainingName = this.trainingService.getCurrentTrainingName();
       const currentExerciseName = this.trainingService.getCurrentExercise().name;
       this.store.addExercisePosition(currentTrainingName, currentExerciseName, newPosition).subscribe({
         next: () => {
           this.dialogRef.close(true);  
-          const information = "Pomyślnie dodano nową pozycję do historii twojego ćwiczenia";
-          this.dialog.open(InfoDialogComponent, {
-            width: '600px',
-            data: { information },
-            enterAnimationDuration,
-            exitAnimationDuration,
-          });
+          this.showInfoDialog("Pomyślnie dodano nową pozycję do historii twojego ćwiczenia", enterAnimationDuration, exitAnimationDuration);
         },
       });
     }
+  }
+
+  private showInfoDialog(information: string, enterAnimationDuration: string, exitAnimationDuration: string): void {
+    const dialogConfig = new MatDialogConfig();
+  
+    dialogConfig.data = { information };
+    dialogConfig.enterAnimationDuration = enterAnimationDuration;
+    dialogConfig.exitAnimationDuration = exitAnimationDuration;
+  
+    if (window.innerWidth <= 768) {
+      dialogConfig.width = '100vw';
+      dialogConfig.height = '100vh';
+      dialogConfig.maxWidth = '100vw';
+      dialogConfig.maxHeight = '100vh';
+    } else {
+      dialogConfig.width = '600px';
+    }
+  
+    this.dialog.open(InfoDialogComponent, dialogConfig);
   }
 
   isValidDate(dateString: string): boolean {
@@ -132,5 +134,11 @@ export class AddExercisePositionDialogComponent {
       return { 'invalidDate': true };
     }
     return null;
+  }
+
+  onClose(): void {
+    if (this.dialogRef) {
+      this.dialogRef.close();
+    }
   }
 }
