5292a841c57f2fef75ac6bf050a79720381bc776
Bartlomiej Socha <batlomiej.socha@mtupolska.com>
2024-06-11 12:36:23 +0200

    removing position function added to details dialog component

--- src/app/training/training-dialogs/exercise-position-details-dialog/exercise-position-details-dialog.component.ts
+++ src/app/training/training-dialogs/exercise-position-details-dialog/exercise-position-details-dialog.component.ts
@@ -3,6 +3,12 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { MatDialog } from '@angular/material/dialog';
+import { TrainingService } from '../../training.service';
+import { TrainingsStore } from '../../../store/trainings.store';
+import { RemovingConfirmDialogComponent } from '../removing-confirm-dialog/removing-confirm-dialog.component';
+import { MatDialogConfig } from '@angular/material/dialog';
+import { InfoDialogComponent } from '../info-dialog/info-dialog.component';
 
 @Component({
   selector: 'app-exercise-position-details-dialog',
@@ -12,7 +18,11 @@ import { ButtonComponent } from '../../../shared/button/button.component';
   styleUrl: './exercise-position-details-dialog.component.scss'
 })
 export class ExercisePositionDetailsDialogComponent {
+
   dialogRef = inject(MatDialogRef);
+  dialog = inject(MatDialog);
+  trainingService = inject(TrainingService);
+  store = inject(TrainingsStore);
   exercise: { name: string; date: string; repetitions: number; weight: number } = inject(MAT_DIALOG_DATA);
 
   onClose() {
@@ -20,6 +30,31 @@ export class ExercisePositionDetailsDialogComponent {
   }
 
   onRemovePosition() {
-    console.log('usunięto pozycję')
+    const messages = {
+      confirmMessage: "Czy na pewno chcesz usunąć wybraną pozycję?",
+      successMessage: "Pomyślnie usunięto wybraną pozycję z historii twojego ćwiczenia"
+    };
+
+    const data = {
+      type: 'position',
+      trainingName: this.trainingService.getCurrentTrainingName(), 
+      exerciseName: this.exercise.name,
+      date: this.exercise.date,
+      messages
+    };
+
+    const confirmDialogRef = this.dialog.open(RemovingConfirmDialogComponent, {
+      data,
+      width: '100vw',
+      height: '100vh',
+      maxWidth: '100vw',
+      maxHeight: '100vh',
+      enterAnimationDuration: '300ms',
+      exitAnimationDuration: '300ms'
+    });
+
+    confirmDialogRef.afterClosed().subscribe(() => {
+      this.dialogRef.close();
+    });
   }
 }
